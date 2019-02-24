055af4528ae232cdbfbb73b21b808ddea701872b
CherieLee-Mason <clsoar@gmail.com>
2019-02-24 16:07:09 -0600

    Book component added

---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Book extends Component {
+  render() {
+    return (
+      <div className="book">
+        <div className="book-image"></div>
+        <div className="book-info">
+          <div className="book-title"></div>
+          <div className="book-author"></div>
+          <div className="book-publishing-co"></div>
+          <div className="book-title"></div>
+        </div>
+        <div className="more-book-info">
+        <a
+          className="book-info-link"
+          href="https://reactjs.org"
+          target="_blank">
+          Learn More
+        </a>
+        </div>
+      </div>
+
+
+
+
+    )
+  }
+}
+
+export default Book
