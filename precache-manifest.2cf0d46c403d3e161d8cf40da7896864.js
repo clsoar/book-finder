6ef7a187d98a4428b6b701b806fa57428934d4e7
CherieLee-Mason <clsoar@gmail.com>
2019-02-26 22:16:19 -0600

    Updates

---
+++ precache-manifest.2cf0d46c403d3e161d8cf40da7896864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "736fd4229ce5946055cf",
+    "url": "/book-finder/static/js/runtime~main.736fd422.js"
+  },
+  {
+    "revision": "da49fe77706d563f2159",
+    "url": "/book-finder/static/js/main.da49fe77.chunk.js"
+  },
+  {
+    "revision": "d6cbbdcb9ddb5bc94f2a",
+    "url": "/book-finder/static/js/2.d6cbbdcb.chunk.js"
+  },
+  {
+    "revision": "da49fe77706d563f2159",
+    "url": "/book-finder/static/css/main.fce79df4.chunk.css"
+  },
+  {
+    "revision": "72e2d66c5cba2b01076b9442e0f1edda",
+    "url": "/book-finder/index.html"
+  }
+];
